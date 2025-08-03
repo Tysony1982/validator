6ac6607607ccb31dee30e4d7b97176e03656cebe
Tysony1982 <96051703+Tysony1982@users.noreply.github.com>
2025-08-03 16:16:30 +1000

    Implement table and column reconciliation validators

--- src/expectations/validators/__init__.py
+++ src/expectations/validators/__init__.py
@@ -4,6 +4,6 @@
 # Pre-load the core validator sub-modules so that `_resolve_validator_class`
 # can quickly locate classes without the calling code needing to specify
 # dotted paths.
-from . import column, table, custom  # noqa: F401
+from . import column, table, custom, reconciliation  # noqa: F401
 # Generate validator schema metadata on package import
 from . import schema  # noqa: F401
